63a98d23710804c56a25d4a8a1ea73cabaf931bf
mattomar <moddathero@gmail.com>
2025-03-20 20:52:11 +0700

    made adjusments for vercel deployment

--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,49 @@
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin"); // Import html-webpack-plugin
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: "./src/index.js", // Specify your entry point
+    entry: "./src/index.js", // Entry point
     output: {
-        path: path.join(__dirname, 'dist'),
-        filename: 'index.bundle.js',
+        path: path.resolve(__dirname, "dist"), // Output directory
+        filename: "index.bundle.js",
+        publicPath: "/", // ✅ Important for React Router
     },
     devServer: {
         port: 3002,
         open: true,
         hot: true,
-        static: path.resolve(__dirname, 'dist'), // Serve content from the 'dist' folder
+        static: path.resolve(__dirname, "dist"),
+        historyApiFallback: true, // ✅ Fixes 404 issues on refresh
     },
     module: {
         rules: [
             {
-                test: /\.js(x?)$/, // Matches .js and .jsx files
-                exclude: /node_modules/, // Exclude node_modules from processing
+                test: /\.js(x?)$/, // Match JS and JSX
+                exclude: /node_modules/,
                 use: {
-                    loader: 'babel-loader', // Use babel-loader to transpile JavaScript and JSX
+                    loader: "babel-loader",
                 },
             },
             {
                 test: /\.css$/i,
-                use: ['style-loader', 'css-loader'], // Process CSS files
+                use: ["style-loader", "css-loader"],
             },
             {
                 test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: 'asset/resource', // Process image files as assets
+                type: "asset/resource",
             },
             {
                 test: /\.(woff|woff2|eot|ttf|otf)$/i,
-                type: 'asset/resource', // Process font files as assets
+                type: "asset/resource",
             },
         ],
     },
     resolve: {
-        extensions: [".js", ".jsx"], // Automatically resolve certain extensions
+        extensions: [".js", ".jsx"],
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: "./src/index.html", // Path to your template file
+            template: "./src/index.html",
         }),
     ],
 };
